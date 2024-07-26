c1eb68f1992301e35c06d00ac7c23a686262363a
mariannacoppa <densimetra@gmail.com>
2024-07-26 17:44:27 +0200

    [FIX] created AppTitle file with h1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
+export default {
+  components: {
+    AppTitle
+  }
+}
 </script>
 
 <template>
-  <div>
-    <h1>La mia prima app con Vite!</h1>
+  <AppTitle/>
+  <!-- <div>
+
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -12,20 +18,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
